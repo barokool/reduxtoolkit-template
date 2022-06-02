0c568b022353df04c58e63430314ae3e199f3930
baro kool <bao.thai@usumsoftware.com>
2022-06-02 18:39:08 +0700

    todolist by redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { useAppDispatch, useAppSelector } from "./redux/hooks/hooks";
+import { v4 as uuidv4 } from "uuid";
+import { addTodo, markComplete, removeTodo } from "./redux/slices/todo";
+import { RootState } from "./redux/storeToolkit";
 function App() {
+  const [todo, setTodo] = useState<string>("");
+
+  const dispatch = useAppDispatch();
+  const { todoList } = useAppSelector((state: RootState) => state);
+  const handleSubmit = () => {
+    // collect todo data and dispatch addTodo
+    const newTodo: Todo = {
+      id: uuidv4(),
+      completed: false,
+      title: todo,
+    };
+    dispatch(addTodo(newTodo));
+    setTodo("");
+  };
+  const handleMarkComplete = (item: Todo) => {
+    dispatch(markComplete(item));
+  };
+  const handleDelete = (item: Todo) => {
+    dispatch(removeTodo(item));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container maxWidth="lg">
+        <Typography align="center" variant="h2" component="h2">
+          ReduxToolkit Todolist
+        </Typography>
+        <Container maxWidth="xs">
+          <TextField
+            id="outlined-basic"
+            label="Type your todo"
+            variant="outlined"
+            fullWidth
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+          />
+          <Button
+            onClick={handleSubmit}
+            variant="contained"
+            color="primary"
+            fullWidth
+          >
+            Add todo List
+          </Button>
+        </Container>
+        <Container className="flex">
+          {todoList?.map((item) => (
+            <div key={item.id} className="flex space-x-2">
+              <input
+                onClick={() => handleMarkComplete(item)}
+                checked={item.completed}
+                type="checkbox"
+              />
+              <p>{item.title}</p>
+              <button
+                className="w-10 bg-red-300"
+                onClick={() => handleDelete(item)}
+              >
+                Delete{" "}
+              </button>
+            </div>
+          ))}
+        </Container>
+      </Container>
     </div>
   );
 }
---
+++ src/redux/slices/todo.ts
@@ -0,0 +1,40 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface ITodo {
+  todoList: Todo[];
+}
+const initialState: ITodo = {
+  todoList: [],
+};
+
+const todoSlice = createSlice({
+  name: "counter",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      state.todoList.push(action.payload);
+    },
+    removeTodo: (state, action: PayloadAction<Todo>) => {
+      const filterTodo = state.todoList.filter(
+        (todo) => todo.id !== action.payload.id
+      );
+      state.todoList = filterTodo;
+    },
+    markComplete: (state, action: PayloadAction<Todo>) => {
+      state.todoList = state.todoList.map((todo) => {
+        if (todo.id === action.payload.id) {
+          todo.completed = !action.payload.completed;
+        }
+        return todo;
+      });
+    },
+  },
+});
+
+// export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { addTodo, removeTodo, markComplete } = todoSlice.actions;
+// Other code such as selectors can use the imported `RootState` type
+
+export default todoSlice.reducer;
+export const todoReducer = todoSlice.reducer;
